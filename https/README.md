25260d3b4cf93d8cce5c3bc60183f734d29a2e13
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2026-02-03 23:11:48 +0100

    feat(https): add https

--- https/README.md
+++ https/README.md
@@ -3,4 +3,5 @@
 ### Create certificates
 
 ```bash
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server.key -out server.cert
 ```
--- https/main.go
+++ https/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-    "fmt"
+	"log"
     "net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "hello\n")
-}
-
 func main() {
-    http.HandleFunc("/hello", hello)
-	fmt.Println("Listening on http://localhost:8080")
-    http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello, HTTPS World with Self-Signed Certificate!"))
+    })
+    certFile := "server.cert"
+    keyFile := "server.key"
+	port := ":8443"
+    log.Println("Starting server at https://localhost" + port)
+    err := http.ListenAndServeTLS(port, certFile, keyFile, nil)
+    if err != nil {
+        log.Fatalf("ListenAndServeTLS failed: %v", err)
+    }
 }
