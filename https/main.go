d195a75c8a625ea13129829e55fad5c9bf002c41
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2026-02-03 23:50:50 +0100

    feat(https): htts files as arguments

--- https/main.go
+++ https/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"flag"
 	"log"
     "net/http"
 )
 
 func main() {
+    certFile := flag.String("cert", "", "Path to the SSL certificate file")
+    keyFile := flag.String("key", "", "Path to the SSL private key file")
+    flag.Parse()
+    if *certFile == "" || *keyFile == "" {
+		log.Fatal(`You must specify both the certificate and key file paths.
+ Example: go run -cert /etc/letsencrypt/live/your-domain.com/fullchain.pem -key /etc/letsencrypt/live/your-domain.com/privkey.pem`)
+    }
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Hello, HTTPS World with Self-Signed Certificate!"))
+		w.Write([]byte("Hello, HTTPS World with Let's Encrypt!"))
     })
-    certFile := "server.cert"
-    keyFile := "server.key"
-	port := ":8443"
+	port := ":443"
     log.Println("Starting server at https://localhost" + port)
-    err := http.ListenAndServeTLS(port, certFile, keyFile, nil)
+    err := http.ListenAndServeTLS(port, *certFile, *keyFile, nil)
     if err != nil {
         log.Fatalf("ListenAndServeTLS failed: %v", err)
     }
